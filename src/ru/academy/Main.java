15552dc20d62e84e041157502a23165018601030
lisin <lisin@DESKTOP-P4T8JEF>
2026-02-02 12:14:36 +1000

    Clear comment

--- src/ru/academy/Main.java
+++ src/ru/academy/Main.java
@@ -12,27 +12,7 @@ import java.nio.file.Path;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-//        InetSocketAddress socketAddress = new InetSocketAddress(8080);
-//        HttpServer httpServer = HttpServer.create(socketAddress, 0);
-//
-//        httpServer.createContext("/", exchange -> {
-//            URI requestUri = exchange.getRequestURI();
-//            System.out.println("Клиент запросил ресурс по адресу:" + requestUri);
-//
-//            Path htmlFilePath = Path.of("Index.html");
-//            byte[] messageAsBytes = Files.readAllBytes(htmlFilePath);
-//
-//            exchange.getResponseHeaders()
-//                    .set("Content-Type", "text/html ; charset=UTF-8");
-//            exchange.sendResponseHeaders(200, messageAsBytes.length);
-//
-//
-//            try (OutputStream outputStream = exchange.getResponseBody();){
-//                outputStream.write(messageAsBytes);
-//            }
-//
-//
-//        });
+
         StaticFileServer staticFileServer = new StaticFileServer(8080);
       staticFileServer.addStaticFilesDirectory(Path.of("static/images"));
       staticFileServer.addStaticFilesDirectory(Path.of("static/text"));
